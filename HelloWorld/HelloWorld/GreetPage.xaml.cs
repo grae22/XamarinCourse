d9446d45050e4ea86350e884cbd4771f2bcf55a1
GraemeB <grae22@gmail.com>
2016-11-22 20:58:46 +0200

    HelloWorld.

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+  public partial class GreetPage : ContentPage
+  {
+    public GreetPage()
+    {
+      InitializeComponent();
+
+      Content = new Label
+      {
+        HorizontalOptions = LayoutOptions.Center,
+        VerticalOptions = LayoutOptions.Center,
+        Text = "Hello"
+      };
+    }
+
+    void OnClick( object sender, System.EventArgs e )
+    {
+      DisplayAlert( "Title", "Message", "OK" );
+    }
+  }
+}
