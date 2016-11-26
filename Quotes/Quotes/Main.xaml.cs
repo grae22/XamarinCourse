ecfd3eaab0c213ba087a79c3f1bb8f738fe58625
GraemeB <grae22@gmail.com>
2016-11-26 21:23:07 +0200

    Quotes.

---
+++ Quotes/Quotes/Main.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Quotes
+{
+  public partial class Main : ContentPage
+  {
+    private List<string> Quotes { get; set; } = new List<string>();
+    private int QuoteIndex { get; set; } = 0;
+
+    public Main()
+    {
+      InitializeComponent();
+
+      Quotes.Add( "Quote 1" );
+      Quotes.Add( "Quote 2" );
+      Quotes.Add( "Quote 3" );
+
+      OnNextClicked( null, null );
+    }
+
+    private void OnNextClicked( object sender, EventArgs args )
+    {
+      uiQuote.Text = Quotes[ QuoteIndex++ ];
+
+      if( QuoteIndex >= Quotes.Count )
+      {
+        QuoteIndex = 0;
+      }
+    }
+  }
+}
