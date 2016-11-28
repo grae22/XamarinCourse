86ae2d473a5fc44d6ddc0cababef9a6a5cfea092
GraemeB <grae22@gmail.com>
2016-11-28 21:43:47 +0200

    Absolute1.

---
+++ Absolute/Absolute/App.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace Absolute
+{
+  public class App : Application
+  {
+    public App()
+    {
+      MainPage = new Absolute1();
+    }
+
+    protected override void OnStart()
+    {
+      // Handle when your app starts
+    }
+
+    protected override void OnSleep()
+    {
+      // Handle when your app sleeps
+    }
+
+    protected override void OnResume()
+    {
+      // Handle when your app resumes
+    }
+  }
+}
