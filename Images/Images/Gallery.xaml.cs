42a62a638cf1d0e1aeb03bf839897d261d68257c
GraemeB <grae22@gmail.com>
2016-12-06 00:02:18 +0200

    Gallery WIP - just need to tweak for horiz phone view.

--- Images/Images/Gallery.xaml.cs
+++ Images/Images/Gallery.xaml.cs
@@ -1,18 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Images
 {
   public partial class Gallery : ContentPage
   {
+    //-------------------------------------------------------------------------
+
+    private const string c_imageBaseAddress = "http://lorempixel.com/1920/1080/city/";
+
+    private int ImageIndex { get; set; } = 1;
+
+    //-------------------------------------------------------------------------
+
     public Gallery()
     {
       InitializeComponent();
+
+      UpdateImage();
     }
+
+    //-------------------------------------------------------------------------
+
+    private void ShowPreviousImage()
+    {
+      ImageIndex--;
+
+      if( ImageIndex < 1 )
+      {
+        ImageIndex = 1;
+      }
+      else
+      {
+        UpdateImage();
+      }
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void ShowNextImage()
+    {
+      ImageIndex++;
+
+      UpdateImage();
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void UpdateImage()
+    {
+      uiImage.Source = c_imageBaseAddress + ImageIndex;
+    }
+
+    //-------------------------------------------------------------------------
   }
 }
